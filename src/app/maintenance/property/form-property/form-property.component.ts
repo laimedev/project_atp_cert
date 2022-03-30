5a5ce385798f90971f65348ac260a01c59e5e5c2
AlexLaime <alaimes64@gmail.com>
2022-03-30 10:31:24 -0500

    develop maps integratirion

--- src/app/maintenance/property/form-property/form-property.component.ts
+++ src/app/maintenance/property/form-property/form-property.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Map, marker, tileLayer } from 'leaflet';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-form-property',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormPropertyComponent implements OnInit {
 
-  constructor() { }
+
+
+  @Input() latitude: any  = '-12.046374';
+  @Input() longitude: any = '-77.0427934';
+  sizeMap:  number = 18;
+
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+
+  ngAfterViewInit(): void {
+    const map = new Map('map').setView([ this.latitude , this.longitude ], this.sizeMap);
+    tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
+      maxZoom: 20,
+      // attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    const markerItem =  marker([this.latitude , this.longitude ]).addTo(map).bindPopup("Sede Lima Centro - Amazonas Trading SAC").openPopup();
+    map.fitBounds([
+      [markerItem.getLatLng().lat, markerItem.getLatLng().lng]
+    ]);
+  }
+
+
+  regresar(){
+    this.router.navigateByUrl(`admin/property.list`);
   }
 
 }
--- src/app/maintenance/property/list-property/list-property.component.ts
+++ src/app/maintenance/property/list-property/list-property.component.ts
@@ -4,7 +4,6 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { PropertyService } from '../services/property.service';
 import { Router } from '@angular/router';
-import { Map, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-list-property',
@@ -14,10 +13,13 @@ import { Map, tileLayer } from 'leaflet';
 export class ListPropertyComponent implements OnInit {
 
   listProducer: any [] = [];
-  displayedColumns: string[] = ['idProperty','nameProperty', 'Address', 'Area', 'Altitude', 'Longitude', 'Wakers', 'options'];
+  displayedColumns: string[] = ['idProperty','nameProperty', 'Address', 'Area', 'Altitude', 'Latitud' ,'Longitude', 'Wakers', 'options'];
   dataSource = new MatTableDataSource(this.listProducer);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+
+  public latitude;
+  public longitude;
 
   constructor(public propertyService: PropertyService,
               public router: Router) { }
@@ -30,17 +32,16 @@ export class ListPropertyComponent implements OnInit {
   }
 
 
-  ngAfterViewInit(): void {
-    const map = new Map('map').setView([51.505, -0.09],13);
-
-    tileLayer('https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png', {
-      maxZoom: 20,
-      attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-  }
+ 
 
   verMas(element){
+
+    // console.log(element.Longitude)
+    // console.log(element.Latitud)
+    
+    // this.latitude = element.Latitud;
+    // this.longitude = element.Longitude;
+
     console.log(element);
     this.router.navigateByUrl(`admin/property.form/${element.idProperty}`);
   }
